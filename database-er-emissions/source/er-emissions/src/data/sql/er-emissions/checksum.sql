ceba80462b32e38838d620312da38ea8d34abd98
luitvm <38038189+eltee1@users.noreply.github.com>
2025-03-14 11:27:53 +0100

    add checksum functionality

---
+++ database-er-emissions/source/er-emissions/src/data/sql/er-emissions/checksum.sql
@@ -0,0 +1,2 @@
+--vul de checksum_hash kolommen voor alle tabelnamen in de tabel metadata
+SELECT * FROM system.fill_metadata_checksum();
---
+++ database-er-emissions/source/er-emissions/src/main/sql/er-emissions/10-public/03-functions/01-general.sql
@@ -0,0 +1,43 @@
+/*
+ * checksum_table
+ * --------------
+ * Functie die een checksum-waarde retourneert voor een gegeven table_name. 
+ * Wordt gebruikt door system.fill_metadata_checksum, om tabellen op inhoud en structuur te kunnen vergelijk tussen databases op verschillende locaties.
+ * @param v_table De tabelnaam waarvan de checksum moet worden gegenereerd.
+ */
+CREATE OR REPLACE FUNCTION system.checksum_table(v_table text)
+	RETURNS SETOF bigint AS
+$BODY$
+BEGIN
+	RETURN QUERY EXECUTE
+		format('SELECT COALESCE(SUM(hashtext((checksum_table.*)::text)), 0)::bigint AS checksum FROM %I AS checksum_table', v_table);
+END;
+	
+$BODY$
+LANGUAGE plpgsql IMMUTABLE;
+
+
+/*
+ * fill_metadata_checksum
+ * ----------------------
+ * Functie die de checksum_hash- kolomn in de metadata-tabel vult aan de hand van de corresponderende table_name kolom, met de waarde zoals gegeven door de functie system.checksum_table.
+ */
+CREATE OR REPLACE FUNCTION system.fill_metadata_checksum()
+	RETURNS VOID AS
+$BODY$
+DECLARE
+	v_table text;
+	v_checksum bigint;
+BEGIN
+	FOR v_table IN 
+		SELECT table_name FROM metadata
+	LOOP
+		v_checksum:= system.checksum_table(v_table);
+		
+		UPDATE metadata SET checksum_hash = v_checksum 
+			WHERE table_name = v_table;
+	END LOOP;
+
+END;
+$BODY$
+	LANGUAGE plpgsql volatile;
