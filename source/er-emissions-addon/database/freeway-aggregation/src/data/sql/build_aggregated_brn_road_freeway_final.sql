83921b5582d1cf3ebf28a1ce16e17cb03bb0322b
luitvm <38038189+eltee1@users.noreply.github.com>
2025-03-26 16:06:21 +0100

    add brn_[sector] tables and views.

--- source/er-emissions-addon/database/freeway-aggregation/src/data/sql/build_aggregated_brn_road_freeway_final.sql
+++ source/er-emissions-addon/database/freeway-aggregation/src/data/sql/build_aggregated_brn_road_freeway_final.sql
@@ -1,9 +1,6 @@
 SELECT ae_raise_notice('Build: agg_aggregated_brn_road_freeway_final per groupid @ ' || timeofday());
 
 
-{multithread on: SELECT groupid FROM freewway.temp_groupids ORDER BY groupid}
+{multithread on: SELECT groupid FROM freeway.temp_groupids ORDER BY groupid}
 	BEGIN; SELECT freeway.build_aggregated_brn_road_freeway_final({groupid}); COMMIT;
 {/multithread}
-
---SELECT unnest(array_agg(generate_series)) AS groupid FROM generate_series(1,24)
---SELECT groupid FROM freewway.temp_groupids ORDER BY groupid
---
+++ source/er-emissions/src/main/sql/er-emissions/10-public/01-tables/06-brn_sectortables.sql
@@ -0,0 +1,196 @@
+/*
+ * brn_agriculture
+ * ---------------
+ * 
+ */
+CREATE TABLE brn_agriculture (
+    snr bigint,
+    x_m bigint,
+    y_m bigint,
+    q_g_s double precision,
+    hc_mw double precision,
+    h_m real,
+    r_m real,
+    s_m real,
+    dv smallint,
+    cat smallint,
+    area smallint,
+    ps smallint,
+    component character varying(5),
+    bronomschrijving character varying(120),
+    basename character varying(50),
+    dataset character varying(450),
+    gcn_sector_id integer,
+    substance_id smallint,
+
+    CONSTRAINT scale_factors_fkey_gcn_sectors FOREIGN KEY (gcn_sector_id) REFERENCES gcn_sectors,
+    CONSTRAINT scale_factorss_fkey_substances FOREIGN KEY (substance_id) REFERENCES substances
+);
+
+CREATE INDEX IF NOT EXISTS brn_agriculture_idx ON brn_agriculture USING btree (gcn_sector_id ASC NULLS LAST, substance_id ASC NULLS LAST);
+CREATE INDEX IF NOT EXISTS brn_agriculture_geom_idx ON public.m25_brn_agriculture USING gist(st_setsrid(st_point((x_m::numeric + 0.1)::double precision, (y_m::numeric + 0.1)::double precision), 28992));
+
+
+/*
+ * brn_industry
+ * ------------
+ * 
+ */
+CREATE TABLE brn_industry (
+    snr bigint,
+    x_m bigint,
+    y_m bigint,
+    q_g_s double precision,
+    hc_mw double precision,
+    h_m real,
+    r_m real,
+    s_m real,
+    dv smallint,
+    cat smallint,
+    area smallint,
+    ps smallint,
+    component character varying(5),
+    bronomschrijving character varying(120),
+    basename character varying(50),
+    dataset character varying(450),
+    gcn_sector_id integer,
+    substance_id smallint,
+
+    CONSTRAINT scale_factors_fkey_gcn_sectors FOREIGN KEY (gcn_sector_id) REFERENCES gcn_sectors,
+    CONSTRAINT scale_factorss_fkey_substances FOREIGN KEY (substance_id) REFERENCES substances
+);
+
+CREATE INDEX IF NOT EXISTS brn_industry_idx ON brn_industry USING btree (gcn_sector_id ASC NULLS LAST, substance_id ASC NULLS LAST);
+CREATE INDEX IF NOT EXISTS brn_industry_geom_idx ON brn_industry USING gist (st_setsrid(st_point((x_m::numeric + 0.1)::double precision, (y_m::numeric + 0.1)::double precision), 28992));
+
+/*
+ * brn_other
+ * ---------
+ * 
+ */
+CREATE TABLE brn_other (
+    snr bigint,
+    x_m bigint,
+    y_m bigint,
+    q_g_s double precision,
+    hc_mw double precision,
+    h_m real,
+    r_m real,
+    s_m real,
+    dv smallint,
+    cat smallint,
+    area smallint,
+    ps smallint,
+    component character varying(5),
+    bronomschrijving character varying(120),
+    basename character varying(50),
+    dataset character varying(450),
+    gcn_sector_id integer,
+    substance_id smallint,
+
+    CONSTRAINT scale_factors_fkey_gcn_sectors FOREIGN KEY (gcn_sector_id) REFERENCES gcn_sectors,
+    CONSTRAINT scale_factorss_fkey_substances FOREIGN KEY (substance_id) REFERENCES substances
+);
+
+CREATE INDEX IF NOT EXISTS brn_other_idx ON brn_other USING btree (gcn_sector_id ASC NULLS LAST, substance_id ASC NULLS LAST);
+CREATE INDEX IF NOT EXISTS brn_other_geom_idx ON brn_other USING gist (st_setsrid(st_point((x_m::numeric + 0.1)::double precision, (y_m::numeric + 0.1)::double precision), 28992));
+
+
+/*
+ * brn_road_freeway
+ * ----------------
+ * 
+ */
+CREATE TABLE brn_road_freeway (
+    snr bigint,
+    x_m bigint,
+    y_m bigint,
+    q_g_s double precision,
+    hc_mw double precision,
+    h_m real,
+    r_m real,
+    s_m real,
+    dv smallint,
+    cat smallint,
+    area smallint,
+    ps smallint,
+    component character varying(5),
+    bronomschrijving character varying(120),
+    basename character varying(50),
+    dataset character varying(450) ,
+    gcn_sector_id integer,
+    substance_id smallint,
+
+    CONSTRAINT scale_factors_fkey_gcn_sectors FOREIGN KEY (gcn_sector_id) REFERENCES gcn_sectors,
+    CONSTRAINT scale_factorss_fkey_substances FOREIGN KEY (substance_id) REFERENCES substances
+);
+
+CREATE INDEX IF NOT EXISTS brn_road_freeway_idx ON brn_road_freeway USING btree (gcn_sector_id ASC NULLS LAST, substance_id ASC NULLS LAST);
+CREATE INDEX IF NOT EXISTS brn_road_freeway_geom_idx ON brn_road_freeway USING gist (st_setsrid(st_point((x_m::numeric + 0.1)::double precision, (y_m::numeric + 0.1)::double precision), 28992));
+
+
+/*
+ * brn_road_transportation
+ * -----------------------
+ * 
+ */
+CREATE TABLE brn_road_transportation (
+    snr bigint,
+    x_m bigint,
+    y_m bigint,
+    q_g_s double precision,
+    hc_mw double precision,
+    h_m real,
+    r_m real,
+    s_m real,
+    dv smallint,
+    cat smallint,
+    area smallint,
+    ps smallint,
+    component character varying(5),
+    bronomschrijving character varying(120),
+    basename character varying(50),
+    dataset character varying(450),
+    gcn_sector_id integer,
+    substance_id smallint,
+
+    CONSTRAINT scale_factors_fkey_gcn_sectors FOREIGN KEY (gcn_sector_id) REFERENCES gcn_sectors,
+    CONSTRAINT scale_factorss_fkey_substances FOREIGN KEY (substance_id) REFERENCES substances
+);
+
+CREATE INDEX IF NOT EXISTS brn_road_transportation_idx ON public.m25_brn_road_transportation USING btree (gcn_sector_id ASC NULLS LAST, substance_id ASC NULLS LAST)
+CREATE INDEX IF NOT EXISTS brn_road_transportation_geom_idx ON public.m25_brn_road_transportation USING gist (st_setsrid(st_point((x_m::numeric + 0.1)::double precision, (y_m::numeric + 0.1)::double precision), 28992));
+
+
+/*
+ * brn_shipping
+ * ------------
+ * 
+ */
+CREATE TABLE IF NOT EXISTS brn_shipping
+(
+    snr bigint,
+    x_m bigint,
+    y_m bigint,
+    q_g_s double precision,
+    hc_mw double precision,
+    h_m real,
+    r_m real,
+    s_m real,
+    dv smallint,
+    cat smallint,
+    area smallint,
+    ps smallint,
+    component character varying(5),
+    bronomschrijving character varying(120),
+    basename character varying(50),
+    dataset character varying(450),
+    gcn_sector_id integer,
+    substance_id smallint,
+
+    CONSTRAINT scale_factors_fkey_gcn_sectors FOREIGN KEY (gcn_sector_id) REFERENCES gcn_sectors,
+    CONSTRAINT scale_factorss_fkey_substances FOREIGN KEY (substance_id) REFERENCES substances
+);
+
+CREATE INDEX IF NOT EXISTS brn_shipping_idx ON public.m25_brn_shipping USING btree (gcn_sector_id ASC NULLS LAST, substance_id ASC NULLS LAST);
+CREATE INDEX IF NOT EXISTS brn_shipping_geom_idx ON public.m25_brn_shipping USING gist (st_setsrid(st_point((x_m::numeric + 0.1)::double precision, (y_m::numeric + 0.1)::double precision), 28992));
