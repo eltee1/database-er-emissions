de96be9fa715bc40853c70187dc593486a3bcd00
luitvm <38038189+eltee1@users.noreply.github.com>
2025-05-13 11:24:29 +0200

    add checksum_check column to checks.metadata

--- source/er-emissions/src/main/sql/common/essentials/02-tables/01-general.sql
+++ source/er-emissions/src/main/sql/common/essentials/02-tables/01-general.sql
@@ -1,14 +1,19 @@
 /*
  * metadata
  * --------
- * Metadata- tabel waarin van elke tabel in de database (schema public) wordt bijgehouden:
- * - welke data-versie is geimporteerd (@column filename_import)
- * - de checksum van de tabel na de import (@column checksum_import), wordt alleen door gebruik van de functie load_table (welke voor dit doeleinde is uitgebreid met deze functionaliteit)
- * - wanneer deze import heeft plaatsgevonden (@column timestamp_import) wordt gevuld door de functie load_table.
- * - de checksum van de tabel na een eventuele wijziging (@column checksum_change), wordt on demand gevuld door de functies checks.checksum_metadata() en checks.checksum_public_tables(). Middels deze
- * kolom is te controleren of de inhoud van de tabel is gewijzigd tov de import van de data via de load_table functie.
- * - wanneer de checksum_change heeft plaatsgevonden (@column timestamp_checksum_change)
- * - opmerkingen tov de inhoud van de taberl (@column remarks)
+ * metadata check-tabel waarin van elke tabel in schema public en brn wordt bijgehouden:
+ * - @column filename_import De data-versie die is geimporteerd via de load_table functie (welke voor dit doeleinde is uitgebreid met deze functionaliteit).
+ * - @column checksum_import De checksum van de tabel na de import, wordt gevuld door functie load_table. Is deze '0', dan bevate de tabel geen data
+ * - @column timestamp_import De datum/tijd van de import, wordt gevuld door functie load_table
+ * - @column checksum_change De checksum van de tabel na handmatige check (door bv de functies check.checksum_*). Is deze '0', dan bevate de tabel geen data
+ * - @column timestamp_checksum_change De datum/tijd van de checksum_change, wordt ook gevuld door de functies check.checksum_*
+ * - @column remarks opmerkingen tov de inhoud van de tabel, kan (tot nu toe) alleen handmatig gevuld worden
+ * - @column checksum_check Vergelijkt checksum_import en checksum_change en geeft hierop een:
+ *		- null (als de vergelijking niet gemaakt kan worden omdat een of beide kolommen [null] bevat iod)
+ *		- TRUE (als beide waarden niet null- en indentiek zijn)
+ *		- FALSE (als beide waarden niet null zijn en verschillen)	 
+ *		Deze kolom wordt on-the-fly gevuld als 1 van de 2 te checken kolommen gewijzigd wordt (door bv checks.checksum_public_brn_tables()). Middels deze
+ * 		kolom is te controleren of de inhoud van de tabel is gewijzigd tov de import van de data via de load_table functie.
  */
 CREATE TABLE checks.metadata (
 	table_name text NOT NULL,
@@ -18,7 +23,18 @@ CREATE TABLE checks.metadata (
 	checksum_change bigint NULL,
 	timestamp_checksum_change text NULL,
 	remarks text NULL,
-	checksum_check boolean GENERATED ALWAYS AS (coalesce(checksum_import::text, '') = coalesce(checksum_change::text,'')) STORED,
+	checksum_check text GENERATED ALWAYS AS (CASE 
+												WHEN 
+													checksum_import IS NOT NULL 
+													AND checksum_change IS NOT NULL 
+													AND (COALESCE(checksum_import::text, '') = COALESCE(checksum_change::text,'')) 
+													THEN 'TRUE' 
+												WHEN 
+													checksum_import IS NOT NULL 
+													AND checksum_change IS NOT NULL 
+													AND (COALESCE(checksum_import::text, '') <> COALESCE(checksum_change::text,'')) 
+													THEN 'FALSE'
+												ELSE '[null]' END) STORED,
 
 	CONSTRAINT metadata_pkey PRIMARY KEY (table_name)
 );
